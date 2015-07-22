2c3b13e42544a8c41f8dbc71005a2467fb6da913
chenlizhan <chung1350@hotmail.com>
2015-07-23 01:10:58 +0800

    Start to using grid

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Happy Birthday 2015 08 02'
+    });
 });
 
 module.exports = router;
