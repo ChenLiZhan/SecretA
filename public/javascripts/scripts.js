efe19e9e419f76779dbd2d41c754bf4a5155e728
chenlizhan <chung1350@hotmail.com>
2015-07-24 00:35:31 +0800

    scrolling

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -8,3 +8,14 @@ $('.expand').click(function(e) {
     e.preventDefault();
     $(this).parent().toggleClass('active');
 });
+
+$(window).bind('scroll', function(e) {
+    parallaxScroll();
+});
+
+function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+    $('#parallax-1').css('top', 500 - (scrolled * 0.75) + 'px');
+    $('#parallax-2').css('top', 1400 - (scrolled * 0.75) + 'px');
+    $('#parallax-3').css('top', 1500 - (scrolled * 0.75) + 'px');
+}
