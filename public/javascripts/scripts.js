47561c2d741b8e2c72ecbaa233ee5f5ed749a6a8
chenlizhan <chung1350@hotmail.com>
2015-07-24 00:43:01 +0800

    Styling and scripting

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,3 @@
-$('.grid').masonry({
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: 200
-});
-
 $('.expand').click(function(e) {
     e.preventDefault();
     $(this).parent().toggleClass('active');
