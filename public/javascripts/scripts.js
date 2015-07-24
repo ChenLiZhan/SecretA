e8c12e57b0dde542611b7b7402c5bdee890f3934
chenlizhan <chung1350@hotmail.com>
2015-07-24 21:51:49 +0800

    Compressed images

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -10,6 +10,6 @@ $(window).bind('scroll', function(e) {
 function parallaxScroll() {
     var scrolled = $(window).scrollTop();
     $('#parallax-1').css('top', 500 - (scrolled * 0.75) + 'px');
-    $('#parallax-2').css('top', 1400 - (scrolled * 0.75) + 'px');
-    $('#parallax-3').css('top', 1500 - (scrolled * 0.75) + 'px');
+    $('#parallax-2').css('top', 1400 - (scrolled * 0.5) + 'px');
+    $('#parallax-3').css('top', 1500 - (scrolled * 0.5) + 'px');
 }
